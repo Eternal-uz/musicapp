731a5834cf5f63bfb45eb93dd2b53c22cc0f30b2
eternal <eternaltalaba@gmail.com>
2021-12-11 18:23:56 +0500

    progress bar

---
+++ src/includes/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBB-mLCsXl2cYcjOgJFL7wUZzOtEcm_naw",
+    authDomain: "music-892be.firebaseapp.com",
+    projectId: "music-892be",
+    storageBucket: "music-892be.appspot.com",
+    appId: "1:812970178368:web:17ee6744371682897eaca5"
+  };
+
+
+
+ firebase.initializeApp(firebaseConfig);
+
+ const auth = firebase.auth();
+ const db = firebase.firestore();
+ const storage = firebase.storage();
+
+ const usersCollection = db.collection('users');
+
+ export { 
+   auth,
+   db,
+   usersCollection,
+   storage,
+ }
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+import store from '@/store';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/manage-music',
+    name: 'manage',
+    meta: {
+      requiresAuth: true
+    },
+    component: Manage,
+    // beforeEnter:(to, from, next)=>{
+    //   console.log('manager route guard');
+    //   next()
+    // }
+  },
+  {
+    path: '/manage',
+    redirect: {name: 'manage'}
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: {name: 'home'}
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'text-yellow-600'
+});
+
+router.beforeEach((to, from, next)=>{
+  if(!to.matched.some((record) => record.meta.requiresAuth)){
+    next();
+    return;
+  }
+  if(store.state.userLoggedIn){
+    next();
+  }else{
+    next({name: 'home'});
+  }
+
+});
+
+export default router;
